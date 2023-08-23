0b790b016ccd36015c86e5278614bc50cd83c77c
cenci-ntido <105450250+cenci-ntido@users.noreply.github.com>
2023-08-23 18:55:36 -0300

    Initial commit

---
+++ server/src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
@@ -0,0 +1,19 @@
+package br.edu.utfpr.pb.pw25s.server.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_user")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String displayName;
+    private String password;
+
+}
---
+++ server/src/test/java/br/edu/utfpr/pb/pw25s/server/UserControllerTest.java
@@ -0,0 +1,82 @@
+package br.edu.utfpr.pb.pw25s.server;
+
+import br.edu.utfpr.pb.pw25s.server.model.User;
+import br.edu.utfpr.pb.pw25s.server.repository.UserRepository;
+import br.edu.utfpr.pb.pw25s.server.shared.GenericResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class UserControllerTest {
+    //methodName_condition_expectedBehavior
+
+    private static final String API_USERS = "/users";
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void cleanup() {
+        userRepository.deleteAll();
+        testRestTemplate.getRestTemplate().getInterceptors().clear();
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_receiveOk() {
+        User user = createValidUser();
+
+        ResponseEntity<Object> response = testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_userSavedToDatabase() {
+
+        User user = createValidUser();
+
+        testRestTemplate.postForEntity(API_USERS, user, Object.class);
+
+        assertThat(userRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_receiveSuccessMessage(){
+        User user = createValidUser();
+        ResponseEntity<GenericResponse> response =
+                testRestTemplate.postForEntity(API_USERS, user, GenericResponse.class);
+        assertThat(response.getBody().getMessage()).isNotNull();
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_passwordIsHashedInDatabase(){
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_USERS, user, Object.class);
+        List<User> users = userRepository.findAll();
+        User userDB = users.get(0);
+        assertThat(userDB.getPassword()).isNotEqualTo(user.getPassword());
+
+    }
+
+    public User createValidUser() {
+        User user = new User();
+        user.setUserName("test-user");
+        user.setDisplayName("test-display");
+        user.setPassword("P4ssword");
+        return user;
+    }
+}
